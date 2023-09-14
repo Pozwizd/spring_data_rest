70f7519b8be53be7e265dbdb62bc81feb1f8ef67
pozwizd <miahkykh.roman@gmail.com>
2023-09-14 14:18:08 +0300

    -

---
+++ src/main/java/com/pozwizd/spring_data_rest/SpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.pozwizd.spring_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/pozwizd/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.pozwizd.spring_data_rest.dao;
+
+
+import com.pozwizd.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
